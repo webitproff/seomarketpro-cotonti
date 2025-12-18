a8d6322bef7c5645f40a7143d9ef7a0e1de5d4ca
sodium carbonate <webitproff@gmail.com>
2025-12-18 10:48:36 +0200

    Update seomarketpro.en.lang.php

--- seomarketpro/lang/seomarketpro.en.lang.php
+++ seomarketpro/lang/seomarketpro.en.lang.php
@@ -3,9 +3,9 @@
  * English Language File for SeoMarketPro plugin for CMF Cotonti Siena v.0.9.26, PHP v.8.4+, MySQL v.8.0
  * Filename: seomarketpro.en.lang.php
  * Purpose: English localization. Defines strings.
- * Date: 17 Dec 2025
+ * Date: Dec 18th, 2025
  * @package SeoMarketPro
- * @version 2.1.1
+ * @version 2.1.2
  * @copyright (c) webitproff 2025 https://github.com/webitproff or https://abuyfile.ccom/users/webitproff
  * @license BSD
  */
@@ -34,7 +34,7 @@ $L['cfg_maxrelated_hint'] = 'other products from the same category will be shown
  */
 $L['info_name'] = 'Seo Market Pro';
 $L['info_desc'] = 'Enhances the SEO capabilities of the Market PRO module in Cotonti by adding meta tags, Open Graph, Twitter Cards, JSON-LD Schema.org structured data, keyword extraction, reading time estimation, and related products functionality.';
-$L['info_notes'] = 'Required: <code>Market PRO module v.5+ by webitproff</code>, PHP 8.4+, MySQL 8.0+, Cotonti Siena v.0.9.26 +';
+$L['info_notes'] = 'Required: <a href="https://github.com/webitproff/marketpro-cotonti" target="_blank"><strong>Market PRO v.5+ by webitproff</strong></a>, PHP 8.4+, MySQL 8.0+, Cotonti Siena v.0.9.26 +';
 
 
 $L['seomarketpro_related'] = 'Related and similar products';
@@ -45,3 +45,4 @@ $L['seomarketpro_unknown_author'] = 'Unknown seller';
  * Stop words for keyword extraction (English)
  */
 $L['seomarketpro_stop_words'] = 'a,about,above,after,again,against,all,am,an,and,any,are,as,at,be,because,been,before,being,below,between,both,but,by,could,did,do,does,doing,down,during,each,few,for,from,further,had,has,have,having,he,her,here,hers,herself,him,himself,his,how,i,if,in,into,is,it,its,itself,just,me,more,most,my,myself,no,nor,not,now,of,off,on,once,only,or,other,our,ours,ourselves,out,over,own,same,she,should,so,some,such,than,that,the,their,theirs,them,themselves,then,there,these,they,this,those,through,to,too,under,until,up,very,was,we,were,what,when,where,which,while,who,whom,why,with,you,your,yours,yourself,yourselves';
+
